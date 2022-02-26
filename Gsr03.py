8d0cc290e4120a469c624a936d3b8dfb5e688cd1
Suhail-GSR <99751317+Suhail-GSR@users.noreply.github.com>
2022-02-26 14:41:26 +0500

    Update and rename test.py to Gsr03.py

--- Gsr03.py
+++ Gsr03.py
@@ -41,9 +41,9 @@ ip = requests.get('https://api.ipify.org').text.strip()
 loc = requests.get('https://ipapi.com/ip_api.php?ip=' + ip, headers={'Referer': 'https://ip-api.com/', 'Content-Type': 'application/json; charset=utf-8', 'User-Agent': 'Mozilla/5.0 (Linux; Android 7.1.2; Redmi 4X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.92 Mobile Safari/537.36'}).json()['country_name'].upper()
 	
 def linex():
-	os.system('echo  "\n ======================================\n" | lolcat -a -d 2 -s 50')
+	os.system('echo  "\n ｡☆✼★━━━━━━═════════════❣═════════════━━━━━━★✼☆｡\n" | lolcat -a -d 2 -s 50')
 def logo():
-	os.system('echo "\n\n  ╔═══════════════════════════════════════════════╗\n     ███████ ██    ██ ██   ██  █████  ██ ██\n     ██      ██    ██ ██   ██ ██   ██ ██ ██      \n     ███████ ██    ██ ███████ ███████ ██ ██      \n          ██ ██    ██ ██   ██ ██   ██ ██ ██      \n          ██ ██    ██ ██   ██ ██   ██ ██ ██      \n     ███████  ██████  ██   ██ ██   ██ ██ ███████      \n  ╚═══════════════════════════════════════════════╝                                  \n    ╔═════════════════════════════╗\n    ║ TOOL NAME: [ HACKER09 ]     ║\n    ║ AUTHOR   : [SUHAIL-GSR]     ║\n    ║ GITHUB   : [git.io/AS]      ║\n    ╚═════════════════════════════╝" | lolcat -a -d 2 -s 50') 
+	os.system('echo "\n\n  ╔═════♥═══════════════════════════════════☆═══════╗\n     ███████ ██    ██ ██   ██  █████  ██ ██\n     ██      ██    ██ ██   ██ ██   ██ ██ ██      \n     ███████ ██    ██ ███████ ███████ ██ ██      \n          ██ ██    ██ ██   ██ ██   ██ ██ ██      \n          ██ ██    ██ ██   ██ ██   ██ ██ ██      \n     ███████  ██████  ██   ██ ██   ██ ██ ███████      \n  ╚═════❦═════════════════════════════════✦═════════╝                                  \n       ╔═════════════════════════════╗\n       ║ TOOL NAME: [  GSR03   ]     ║\n       ║ AUTHOR   : [SUHAIL-GSR]     ║\n       ║ GITHUB   : [git.io/AS]      ║\n    ╚═════════════════════════════╝" | lolcat -a -d 2 -s 50') 
 
 def main():
 	os.system("clear")
